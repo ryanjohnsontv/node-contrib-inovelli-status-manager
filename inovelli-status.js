8246cbf240c18a8efdc1c7d93638c24b8342da26
Ryan Johnson <ryanjohnson95@gmail.com>
2021-03-26 13:08:01 -0500

    Added more input formats and error handling

--- inovelli-status.js
+++ inovelli-status.js
@@ -8,7 +8,7 @@ module.exports = function (RED) {
     this.zwave = zwave;
     this.entityid = entityid;
     this.nodeid = parseInt(nodeid, 10);
-    this.color = color;
+    this.color = parseInt(color, 10);
     this.level = parseInt(level, 10);
     this.duration = parseInt(duration, 10);
     this.effect = parseInt(effect, 10);
@@ -26,18 +26,24 @@ module.exports = function (RED) {
         switchtype: presetSwitchtype,
       } = node;
       const { payload } = msg;
-
+      const zwave = payload.zwave || presetZwave;
+      const level = payload.brightness || presetLevel;
       var color = payload.color || presetColor;
-      if (isNaN(parseInt(color)) === false && typeof color != "object") {
-        color = parseInt(color);
-      }
-      var rgb = [255, 0, 0];
-      var inputconvert = function (color) {
+      var duration = payload.duration || presetDuration;
+      var effect = payload.effect || presetEffect;
+      var switchtype = payload.switchtype || presetSwitchtype;
+      var error = 0;
+
+      var inputColorConvert = function (color) {
+        //if (isNaN(parseInt(color)) === false && typeof color != "object") {
+        //  color = parseInt(color);
+        //}
         if (Array.isArray(color) && typeof color === "object") {
           if (color.length === 3) {
             rgb = color;
           } else {
             node.error(`Check your RGB values: ${color}`);
+            error ++
           }
         } else if (typeof color === "string") {
           if (color.startsWith("#") === true) {
@@ -52,46 +58,129 @@ module.exports = function (RED) {
             rgb = convert.hsv.rgb(conv_hsv);
           } else {
             node.error(`Incorrect Hue Value: ${color}`);
+            error ++
           }
+        } else {
+          node.error(`Incorrect Color: ${color}. Using default color: Red`);
         }
         return rgb;
       };
-      inputconvert(color);
-      if (rgb === undefined) {
-        node.error(`Incorrect Color: ${color}`);
-      }
+
+      var inputSwitchConvert = function (switchtype) {
+        if (isNaN(switchtype)) {
+          const switchType = {
+            "switch":8,
+            "dimmer":16,
+            "combo_light":24,
+            "combo_fan":25
+          }
+          let switchConvert = switchType[switchtype];
+          if (switchConvert !== undefined) {
+            switchtype = switchConvert;
+          }
+        } else if (![8,16,24,25].includes(switchtype)) {
+          node.error(`Incorrect Switch Type: ${switchtype}`);
+          error ++
+        }
+        return switchtype;
+      };
+      
+      var inputDurationConvert = function (duration) {
+        if (isNaN(duration)) {
+          let value = parseInt(duration);
+          let unit = duration.replace(/^[\s\d]+/, '').toLowerCase();
+          if (["second","seconds"].includes(unit) && value >= 0 && value <= 60) {
+            duration = value;
+          } else if (["minute","minutes"].includes(unit) && value > 0 && value <= 60) {
+            duration = value + 60;
+          } else if (["hour","hours"].includes(unit) && value > 0 && value <= 134) {
+            duration = value + 120;
+          } else if (["day","days"].includes(unit) && value > 0 && value <= 5) {
+            duration = (value * 24) + 120;
+          } else if (["forever","indefinite","indefinitely"].includes(unit)) {
+            duration = 255;
+          } else if (["off","silent"].includes(unit)) {
+              duration = 0;
+          } else {
+            node.error(`Incorrect Duration Format: ${duration}`);
+            error ++
+          }
+        } else if (duration < 0 || duration > 255) {
+          node.error(`Incorrect Duration: ${duration}`);
+          error ++
+        }
+        return duration;
+      };
+
+      var inputEffectConvert = function (effect, switchtype) {
+        if (isNaN(effect)) {
+          const switchOptions = {
+            "Off":0,
+            "Solid":1,
+            "Fast Blink":2,
+            "Slow Blink":3,
+            "Pulse":4
+          };
+          const dimmerOptions = {
+            "Off":0,
+            "Solid":1,
+            "Chase":2,
+            "Fast Blink":3,
+            "Slow Blink":4,
+            "Pulse":5
+          };
+          if (switchtype === 8 && switchOptions[effect] !== undefined) {
+            effect = switchOptions[effect];
+          } else if ([16,24,25].includes(switchtype) && dimmerOptions[effect] !== undefined) {
+            effect = dimmerOptions[effect];
+          } else {
+            node.error(`Incorrect Effect: ${effect}`);
+            error ++
+          }
+        } else if (![0,1,2,3,4,5].includes(effect)) {
+          node.error(`Incorrect Effect: ${effect}`);
+          error ++
+        }
+        return effect;
+      };
+
+      switchtype = inputSwitchConvert(switchtype);
+      inputColorConvert(color);
+      duration = inputDurationConvert(duration);
+      effect = inputEffectConvert(effect, switchtype);
+
+      
       const hsl = convert.rgb.hsl(rgb);
       const keyword = convert.rgb.keyword(rgb);
       color = parseInt((hsl[0] * (17 / 24)).toFixed(0));
-      const level = payload.level || presetLevel;
-      const duration = payload.duration || presetDuration;
-      const effect = payload.effect || presetEffect;
-      const zwave = payload.zwave || presetZwave;
-      const switchtype = payload.switchtype || presetSwitchtype;
-      if (zwave === "zwave_js") {
-        const entity_id = payload.entity_id || entityid;
-        if (entity_id === undefined || entity_id === "") {
+      if (error === 0) {
+        if (zwave === "zwave_js") {
+          const entity_id = payload.entity_id || entityid;
+          if (entity_id === undefined || entity_id === "") {
+            node.send({
+              ...msg,
+              payload: { data: { parameter: switchtype, value: level } }
+            });
+          } else {
+            node.send({
+              ...msg,
+              payload: { target:{ entity_id }, data: { parameter: switchtype, value: level } }
+            });
+          }
+        } else if (zwave === "ozw") {
+          const value = color + (level * 255) + (duration * 65536) + (effect * 16777216);
+          const nodeId = payload.node_id || nodeid;
+          const node_id = nodeId ? { node_id: nodeId } : {};
           node.send({
             ...msg,
-            payload: { data:{parameter: switchtype,value: level} }
-          });
-        } else {
-          node.send({
-            ...msg,
-            payload: { target:{ entity_id }, data:{parameter: switchtype,value: level} }
+            payload: { data: {...node_id, parameter: switchtype, value} },
           });
         }
-      } else if (zwave === "ozw") {
-        const value = color + (level * 255) + (duration * 65536) + (effect * 16777216);
-        const nodeId = payload.node_id || nodeid;
-        const node_id = nodeId ? { node_id: nodeId } : {};
-        node.send({
-          ...msg,
-          payload: { ...node_id, parameter: switchtype, value },
-        });
-      }
 
-      node.status({ text: `Sent color: ${keyword}` });
+        node.status({ text: `Sent color: ${keyword}` });
+      } else {
+        node.status({ text: `Error! Check debug window for more info` });
+      }
     });
   }
   RED.nodes.registerType("inovelli-status-manager", InovelliStatusManager);
